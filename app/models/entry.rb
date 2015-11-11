d8c2a3f8649208c77ad49e350132d31dcd8db871
Adam Lopez <Adamf04@gmail.com>
2015-11-11 16:10:24 -0500

    changed entry validator to one line

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,10 +2,12 @@ class Entry < ActiveRecord::Base
 	belongs_to :project
 	validates(:hours, {numericality: true})
 	validates(:minutes, {numericality: true})
-	
-	# validates([ :date, :hours, :minutes ], { presence: true})
 
-	validates(:date, { presence: true})
-	validates(:hours, { presence: true})
-	validates(:minutes, { presence: true})
+	validates(:date, :hours, :minutes, { presence: true})
+
+	#this ^^ works instead of that vv
+
+	# validates(:date, { presence: true})
+	# validates(:hours, { presence: true})
+	# validates(:minutes, { presence: true})
 end
